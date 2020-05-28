9c81fd847762e2f2ef3463a499a30caefef174be
nw <nico.walters@techtalents.es>
2020-05-28 18:32:09 +0200

    Dice rolling output

--- dice_roller.py
+++ dice_roller.py
@@ -18,15 +18,15 @@ def setup_window():
     window.columnconfigure(1, weight=1)
     window.rowconfigure(1, weight=1)
 
-    number_ouput = tk.Label(window, text="Click the button to roll")
+    number_output = tk.Label(window, text="Click the button to roll")
     button = tk.Button(window, text="Roll die", command=handle_click)
 
-    number_ouput.grid(row=1, column=1)
+    number_output.grid(row=1, column=1)
     button.grid(row=1, column=0)
 
-    return window
+    return window, number_output
 
 
 if __name__ == "__main__":
-    window = setup_window()
+    window, number_output = setup_window()
     window.mainloop()
