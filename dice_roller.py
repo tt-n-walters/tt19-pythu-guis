78d7ee9020088f642a5b035beadd62d326bb1b85
nw <nico.walters@techtalents.es>
2020-05-28 18:22:30 +0200

    Grid layout

--- dice_roller.py
+++ dice_roller.py
@@ -8,12 +8,16 @@ def roll_dice(max=6):
 
 def setup_window():
     window = tk.Tk()
+    window.geometry("300x50")
+    window.columnconfigure(0, weight=1)
+    window.columnconfigure(1, weight=1)
+    window.rowconfigure(1, weight=1)
 
     number_ouput = tk.Label(window, text="Click the button to roll")
     button = tk.Button(window, text="Roll die")
 
-    number_ouput.pack()
-    button.pack()
+    number_ouput.grid(row=1, column=1)
+    button.grid(row=1, column=0)
 
     return window
 
