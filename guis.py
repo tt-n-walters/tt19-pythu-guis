61ec1f3744d412007a039011946f2b00deb5ee06
nw <nico.walters@techtalents.es>
2020-05-28 18:04:24 +0200

    Button

--- guis.py
+++ guis.py
@@ -7,5 +7,10 @@ window.geometry("400x300")
 my_label = Label(window, text="Hello world!", fg="yellow", bg="black")
 my_label.pack()
 
+def button_click():
+    print("Oh my! You clicked on the button!")
+
+my_button = Button(window, text="Click me!", command=button_click)
+my_button.pack()
 
 window.mainloop()
